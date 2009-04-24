95c69493e93f0c8120efe2f2947a7fe0d5bb025a
Geoff Garside <geoff@geoffgarside.co.uk>
2009-04-24 16:36:55 +0100

    Refactor Meter::Raritan to better method names

--- lib/ropian/meter/raritan.rb
+++ lib/ropian/meter/raritan.rb
@@ -8,19 +8,19 @@ module Ropian
         @manager = SNMP::Manager.new(:Host => ip_addr,
           :Community => community, :Version => version)
       end
-      # MilliAmps in total on whole bar
+      # Amps in total on whole bar
       def total_amps
         oid = SNMP::ObjectId.new("1.3.6.1.4.1.13742.4.1.4.2.1.3.1")
         amps_for_oid(oid)
       end
-      # MilliAmps per port
-      def amps(port_index)
-        oid = SNMP::ObjectId.new("1.3.6.1.4.1.13742.4.1.2.2.1.4") + port_index
+      # Amps per outlet
+      def outlet_amps(outlet_index)
+        oid = SNMP::ObjectId.new("1.3.6.1.4.1.13742.4.1.2.2.1.4") + outlet_index
         amps_for_oid(oid)
       end
       protected
         def amps_for_oid(oid)
-          @manager.get(oid).varbind_list.first.value.to_f
+          @manager.get(oid).varbind_list.first.value.to_f / 1000
         end
     end
   end
