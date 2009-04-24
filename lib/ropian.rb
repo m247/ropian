1ef205351da5fbe4c5aa831d7d9c7bd5f7b2ee91
Geoff Garside <geoff@geoffgarside.co.uk>
2009-04-24 15:52:04 +0100

    Add ropian.rb initialisation file

---
+++ lib/ropian.rb
@@ -0,0 +1,31 @@
+require 'ropian/control/generic'
+require 'ropian/control/raritan'
+require 'ropian/control/apc'
+
+require 'ropian/meter/raritan'
+
+require 'snmp'
+
+# :stopdoc:
+class SNMP::Manager
+  attr_reader :host, :community, :version
+end
+class SNMP::ObjectId
+  def +(val)
+    current = self.to_a
+    unless val.kind_of?(Array)
+      current << val.to_i
+      return self.class.new(current)
+    else
+      return self.class.new(current + val.to_a)
+    end
+  end
+end
+# :startdoc:
+
+module Ropian # :nodoc:
+  module Control # :nodoc:
+  end
+  module Meter # :nodoc:
+  end
+end
