70a5c72d3f69c6b0962e74c7be6b83dff6da42cf
Geoff Garside <geoff@geoffgarside.co.uk>
2009-04-29 12:16:36 +0100

    Add collection method for gathering all outlet details at once

--- lib/ropian/meter/raritan.rb
+++ lib/ropian/meter/raritan.rb
@@ -1,6 +1,9 @@
 module Ropian
   module Meter
     class Raritan
+      UnitCurrent = SNMP::ObjectId.new("1.3.6.1.4.1.13742.4.1.4.2.1.3.1")
+      OutletCurrent = SNMP::ObjectId.new("1.3.6.1.4.1.13742.4.1.2.2.1.4")
+
       def inspect
         "#<#{self.class} #{@manager.host}@#{@manager.community}>"
       end
@@ -8,15 +11,24 @@ module Ropian
         @manager = SNMP::Manager.new(:Host => ip_addr,
           :Community => community, :Version => version)
       end
+      def collect # :yields: total_amps, hash_of_outlet_amps
+        results = Hash.new
+
+        @manager.walk(OutletCurrent) do |r|
+          r.each do |varbind|
+            results[varbind.name.index(OutletCurrent)] = varbind.value.to_f / 1000
+          end
+        end
+
+        yield total_amps, results
+      end
       # Amps in total on whole bar
       def total_amps
-        oid = SNMP::ObjectId.new("1.3.6.1.4.1.13742.4.1.4.2.1.3.1")
-        amps_for_oid(oid)
+        amps_for_oid(UnitCurrent)
       end
       # Amps per outlet
       def outlet_amps(outlet_index)
-        oid = SNMP::ObjectId.new("1.3.6.1.4.1.13742.4.1.2.2.1.4") + outlet_index
-        amps_for_oid(oid)
+        amps_for_oid(OutletCurrent + outlet_index)
       end
       protected
         def amps_for_oid(oid)
